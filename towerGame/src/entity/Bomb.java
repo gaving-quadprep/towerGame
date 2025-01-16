e757b776b96292a57842a05497e63491adf19c80
gaving-quadprep <gaving@quadprep.org>
2025-01-16 14:18:57 -0500

    change version number

--- towerGame/src/entity/Bomb.java
+++ towerGame/src/entity/Bomb.java
@@ -7,7 +7,7 @@ import map.Level;
 import util.CollisionChecker;
 
 public class Bomb extends LivingEntity {
-	private boolean dying = false;
+	private boolean goingToExplode = false;
 	public Bomb(Level level) {
 		super(level);
 		this.hitbox = CollisionChecker.getHitbox(1, 2, 15, 16);
@@ -21,8 +21,8 @@ public class Bomb extends LivingEntity {
 	}
 	
 	public void onDied() {
-		if(!dying) {
-			dying = true;
+		if(!goingToExplode) {
+			goingToExplode = true;
 			Explosion explosion = new Explosion(level);
 			level.addEntity(explosion);
 			explosion.setPosition(x + 0.5, y + 0.5);
--- towerGame/src/main/Main.java
+++ towerGame/src/main/Main.java
@@ -45,7 +45,7 @@ public abstract class Main {
 	public static int screenHeight = 240 * scale;
 	public static int width = (int) Math.ceil((double) (screenWidth / tileSize));
 	public static int height = (int) Math.ceil((double) (screenHeight / tileSize));
-	public static final String version = "0.6.3";
+	public static final String version = "0.6.4";
 	
 	public static final WorldRenderer worldRenderer = new WorldRenderer();
 	private static JFrame frame;
