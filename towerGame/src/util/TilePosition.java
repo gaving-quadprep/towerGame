2b6aa37797c6edd85d4a29e0aca019ff1fa19b1e
gaving-quadprep <gaving@quadprep.org>
2025-02-26 10:32:19 -0500

    code cleanup

---
+++ towerGame/src/util/TilePosition.java
@@ -0,0 +1,11 @@
+package util;
+
+public final class TilePosition {
+	public int x;
+	public int y;
+	public TilePosition() {}
+	public TilePosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
